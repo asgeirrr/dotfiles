2b732b2807be7b4c629b1756135dc962c6e544f2
Oskar Hollmann <oskar@hollmann.me>
2024-07-15 14:22:26 +0200

    wezterm: Use F11 as fullscreen shortcut

--- wezterm.lua
+++ wezterm.lua
@@ -5,9 +5,6 @@ local wezterm = require 'wezterm'
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
--- This is where you actually apply your config choices
-
--- For example, changing the color scheme:
 config.hide_tab_bar_if_only_one_tab = true
 config.font = wezterm.font 'Hack'
 config.window_padding = {
@@ -15,6 +12,20 @@ config.window_padding = {
   right = 2,
   top = 2,
   bottom = 2,
+}
+
+-- Shortcuts
+config.keys = {
+  {
+    key = 'F11',
+    action = wezterm.action.ToggleFullScreen,
+  },
+}
+
+config.unix_domains = {
+  {
+    name = 'dropdown',
+  }
 }
 
 -- and finally, return the configuration to wezterm
